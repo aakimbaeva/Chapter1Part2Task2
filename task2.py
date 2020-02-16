20a3d787170b2f0ef0357263710c7f8e00d78435
Azhara <mac@MacBook-Air-mac-AJara.local>
2020-02-16 23:15:37 +0600

    first commit

---
+++ task2.py
@@ -0,0 +1,10 @@
+students = int(input('Students: '))
+apples = int(input('Apples: '))
+print('Each student will get', apples//students, 'apples', 'and', apples % students, 'apples in the basket')
+
+#def get(apples, students):
+#    result1 = apples // students
+#    result2 = apples % students
+#    print(f'Each student will get {result1} apple(s) and {result2} apple(s) will remain in the basket')
+
+#get(21,10)
